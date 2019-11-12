2c400921d1025bb8abc885a2d1ac73357bec803c
Sreekanth <sreekanthj009@gmail.com>
2019-11-12 23:16:50 +0530

    layout code

--- src/components/pages/landing-page.js
+++ src/components/pages/landing-page.js
@@ -1,19 +1,41 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+// import FormRow from '@material-ui/core/FormRow';
 import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
 
 class LandingPage extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {}
   }
 
-  render(){
+  render() {
     return (
       <React.Fragment>
-        <Container maxWidth="sm">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        <Container className={'emi-calci'} maxWidth="md" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
+          <Grid container spacing={1}>
+            <Grid container item xs={12} sm={4} md={4} spacing={4}>
+              <form noValidate autoComplete="off">
+                <TextField 
+                  id="standard-secondary"
+                  label="Loan Amount" 
+                  color="secondary"
+                />
+                <TextField
+                  id="standard-secondary"
+                  label="Tenure (Years)" 
+                  color="secondary"
+                />
+                <TextField
+                  id="standard-secondary"
+                  label="Rate on Interest (%)" 
+                  color="secondary"
+                />
+              </form>
+            </Grid>
+          </Grid>
         </Container>
       </React.Fragment>
     )
