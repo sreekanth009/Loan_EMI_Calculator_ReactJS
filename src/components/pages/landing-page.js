13117c6c4dbac6259ea0e0523c98b35087d91600
Sreekanth <sreekanthj009@gmail.com>
2019-11-27 23:22:28 +0530

    some more changes

--- src/components/pages/landing-page.js
+++ src/components/pages/landing-page.js
@@ -20,6 +20,7 @@ class LandingPage extends React.Component {
       tenure: '',
       rateOfInt: '',
       monthlyEmiAmount: '',
+      resultNumbers: {},
     }
     this.calculateEmi = this.calculateEmi.bind(this);
     this.inputHandleChange = this.inputHandleChange.bind(this);
@@ -47,27 +48,56 @@ class LandingPage extends React.Component {
     });
   }
 
+  // Number formatting 
+  numberFormat = (num) => {
+    return num.toString().replace(/(\d)(?=(\d\d)+\d$)/g, "$1,");
+    // return num.replace(/['"]+/g, '');
+  }
+
   // Reset input fields
   resetFields = (e) => {
     e.preventDefault();
-    alert('4445')
-    const {
-      loanAmount,
-      rateOfInt,
-      tenure,
-      monthlyEmiAmount
-    } = this.state;
-
     this.setState({
       loanAmount: '',
       rateOfInt: '',
       tenure: '',
       monthlyEmiAmount: ''
     })
-    document.querySelector('input[name=loanAmount]').value = '';
+    const allInputFields = document.querySelectorAll('input');
+    const fieldsLength = allInputFields.length;
+    for (var i = 0; i < fieldsLength; ++i) {
+      allInputFields[i].value = '';
+    }
   }
 
   render() {
+    const {
+      loanAmount,
+      tenure,
+      monthlyEmiAmount,
+      resultNumbers
+    } = this.state;
+
+    const totalAmt = Math.floor(monthlyEmiAmount ? monthlyEmiAmount * (tenure*12) : 0);
+    const interestAmt = Math.floor(totalAmt ? totalAmt - loanAmount : 0);
+
+    // // Formatting numbers
+    // const loanAmt = this.numberFormat(loanAmount);
+    // const formatMonthlyEmi = this.numberFormat(monthlyEmiAmount);
+    // const formatTotalAmt = this.numberFormat(totalAmt);
+    // const formatinterestAmt = this.numberFormat(interestAmt);
+
+    // this.setState({
+    //   resultNumbers: Object.assign(resultNumbers, {
+    //     loanAmt: this.numberFormat(loanAmount),
+    //     formatMonthlyEmi: this.numberFormat(monthlyEmiAmount),
+    //     formatTotalAmt: this.numberFormat(totalAmt),
+    //     formatinterestAmt: this.numberFormat(interestAmt)
+    //   })
+    // })
+
+    // console.log('resultNumbers', resultNumbers);
+
     return (
       <React.Fragment>
         <Container p={0} className={'emi-calci'} maxWidth="md" style={{ backgroundColor: '#cfe8fc', height: '95vh' }}>
@@ -97,7 +127,10 @@ class LandingPage extends React.Component {
                   onChange={this.inputHandleChange}
                 />
                 <div className={'operation-btns'}>
-                  <Button variant="contained" color="primary">
+                  <Button variant="contained"
+                    color="primary"
+                    onClick={this.calculateEmi}
+                  >
                     Calculate
                   </Button>
                   <Button variant="contained" color="secondary" onClick={this.resetFields}>
